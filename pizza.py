5ac48cad6caffdd98e03163be2d0c09bd92bd30a
SoSorryTT <markpanya@gmail.com>
2021-10-11 11:57:56 +0700

    Did as readme

--- pizza.py
+++ pizza.py
@@ -1,3 +1,19 @@
+from enum import Enum
+
+class PizzaSize(Enum):
+    # Enum members written as: name = value
+    small = 120
+    medium = 200
+    large = 280
+    jumbo = 400
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def price(self):
+       return self.value
+
 class Pizza:
     """A pizza with a size and optional toppings."""
 
@@ -5,19 +21,24 @@ class Pizza:
         self.size = size
         self.toppings = []
 
-    def getPrice(self):
-        """Price of pizza depends on size and number of toppings."""
-        if self.size == 'small':
-            price = 120 + 20*len(self.toppings)
-        elif self.size == 'medium':
-            price = 200 + 20*len(self.toppings)
-        elif self.size == 'large':
-            price = 280 + 20*len(self.toppings)
+    def __str__(self):
+        # create printable description of the pizza such as
+        # "small pizza with muschroom" or "small plain pizza"
+        description = self.size.name
+        if self.toppings:
+            description += " pizza with "+ ", ".join(self.toppings)
         else:
-            raise ValueError("Unknown pizza size "+self.size)
+            description += " plain pizza"
+        return description
+
+    def get_price(self):
+        """Price of pizza depends on size and number of toppings."""
+        price = self.size.price + 20*len(self.toppings)
+        # else:
+        #     raise ValueError("Unknown pizza size "+self.size)
         return price
     
-    def addTopping(self, topping):
+    def add_topping(self, topping):
         """Add a topping to the pizza"""
         if topping not in self.toppings:
             self.toppings.append(topping)
